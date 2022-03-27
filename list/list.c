c3e2a24c8bbe79e99b7e174c80f6f19020b411c8
Michael Bartlett <michael@ou.edu>
2022-03-27 16:57:28 -0500

    Read in and open files

--- list/list.c
+++ list/list.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
-int main(void) 
+#include <stdlib.h>
+
+//Getting arguments from command line code from https://www.cs.utah.edu/~germain/PPS/Topics/C_Language/command_line_args.html
+int main( int num_args, char* list_of_args[])
 {
 	//File opening and writing code from https://www.cs.utah.edu/~germain/PPS/Topics/C_Language/file_IO.html
-	FILE* output = fopen("output.out", "w");
-	// test for files not existing. 
-	if (output == NULL)
+	//Open the input and output files
+	FILE* input_file = fopen(list_of_args[1], "r");
+	FILE* output_file = fopen(list_of_args[2], "w");
+
+	//Test for files not existing. 
+	if (!input_file || !output_file)
 	{
 		printf("Error! Could not open file\n");
+		exit(-1);
 	}
 
-	// write to file vs write to screen 
-	fprintf(output, "this is a test %d\n", 2); // write to file
+	//Read from the file and write it to the output
+	char line[100];
+	while (fgets(line, 100, input_file) != NULL)
+	{
+		printf("Line: %s", line);
+	}
 
-	printf("End of code\n");
+	//Return 0
 	return (0);
 }
